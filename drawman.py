2b8f017731a2bad2220083df1cb710815ffbc1ef
takon76 <takon76@mail.ru>
2017-04-24 12:28:30 +0300

    Исправили ошибку изменения масштаба Чертежника.  Когда менялся масштаб извне модуля, нельзя было изменить глобальную переменную модуля.  Теперь изменение мастаба оформлено как вызов функции

--- drawman.py
+++ drawman.py
@@ -1,5 +1,5 @@
 from turtle import Turtle   #чертежник из черепахи
-
+default_scale = 10
 
 def init_drawman():
     global t, x_cur, y_cur, drawman_scale
@@ -8,9 +8,12 @@ def init_drawman():
     x_cur = 0
     y_cur = 0
     t.goto(x_cur, y_cur)
-    drawman_scale = 10
+    drawman_scale(default_scale)
 
 
+def drawman_scale(scale):
+    global _drawman_scale
+    _drawman_scale = scale
 
 def test_drawman():
     pen_down()
@@ -37,7 +40,7 @@ def to_point(x,y):
     global x_cur, y_cur
     x_cur = x
     y_cur = y
-    t.goto(drawman_scale *x,drawman_scale *y)
+    t.goto(_drawman_scale *x,_drawman_scale *y)
 
 
 init_drawman()
