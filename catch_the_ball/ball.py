9e688b063c81ba9e077539d7d57ec1aee80b0301
takon76 <takon76@mail.ru>
2017-04-28 17:24:43 +0300

    прог

--- catch_the_ball/ball.py
+++ catch_the_ball/ball.py
@@ -18,16 +18,28 @@ def print_hello(event):
         raise ValueError()
 
 
-root = tkinter.Tk()
+def init_main_window():
+    global root, button1, button2, label, text, scale
 
-button1 = tkinter.Button(root, text="Button 1", command=button1_command)  #создаем кнопку с названием
-button1.bind("<Button>",print_hello)
-button1.pack()  #упаковываем
+    root = tkinter.Tk()
 
-button2 = tkinter.Button(root, text = "Button 2")  #создаем кнопку с названием
-button2.bind("<Button>",print_hello)  #создаем события работы
-button2.pack()  #упаковываем
+    button1 = tkinter.Button(root, text="Button 1", command=button1_command)  #создаем кнопку с названием
+    button1.bind("<Button>",print_hello)
+
+    button2 = tkinter.Button(root, text = "Button 2")  #создаем кнопку с названием
+    button2.bind("<Button>",print_hello)  #создаем события работы
+
+    variable = tkinter.IntVar(0)
+    label = tkinter.Label(root,textvariable=variable)
+    scale = tkinter.Scale(root, orient=tkinter.horizontal,length=300,
+                          from_ =0, to=100, tickinterval=10, resolution=5,variable=variable)
+    text = tkinter.Entry(root, textvariable=variable)
+
+    for obj in button1, button2, label, scale, text:
+        obj.pack()
 
 
+if __name__ == "_main_":
+    init_main_window()
 
-root.mainloop()
+    root.mainloop()
